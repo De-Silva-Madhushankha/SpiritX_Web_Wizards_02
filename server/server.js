b4131bddb4a8e849c08fe46c9f77d205c34e23bb
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2025-03-09 13:26:53 +0530

    cors update

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ const io = new Server(server, {
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","http://localhost:5174"], 
   credentials: true,
 }));
 app.use(cookieParser());
