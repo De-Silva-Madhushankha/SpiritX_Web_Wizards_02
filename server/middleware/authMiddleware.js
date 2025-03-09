a6685dc69a388c9dd1dc3bf715393f4fe677a5a2
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2025-03-09 18:58:59 +0530

    updated auth middleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -8,17 +8,14 @@ const authMiddleware = async (req, res, next) => {
       return res.status(401).json({ message: "No token provided" });
     }
     const decoded = verifyToken(token);
-    console.log('decoded:', decoded);
     const user_id = decoded.id; 
     const user = await User.findById(user_id); // Find the user by ID
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
     req.user = user;
-    console.log('authmiddle:', user);
     next(); 
   } catch (error) {
-    console.error('Error in authMiddleware:', error);
     return res.status(401).json({ message: "Invalid or expired token", error: error.message });
   }
 };
